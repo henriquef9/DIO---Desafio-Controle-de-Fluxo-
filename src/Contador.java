21aa96081249175f016cc48a4ab091e40e98e9ed
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-08-03 16:59:56 -0300

    initial commit

---
+++ src/Contador.java
@@ -0,0 +1,40 @@
+import exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        Scanner terminal  = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal .nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal .nextInt();
+
+        try{
+            contar(parametroUm, parametroDois);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+
+        int contador = parametroDois - parametroUm;
+
+        for(int i = 1; i <= contador; i++){
+            System.out.println(
+                    "Imprimindo o número "+i
+            );
+        }
+
+    }
+
+}
